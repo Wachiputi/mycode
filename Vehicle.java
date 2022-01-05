4bc9ae910b0896a62a366ed49badd9740cac18f7
wachiputi <bsc-ele-29-19@unima.ac.mw>
2022-01-05 12:39:24 +0200

    this is a parent class

---
+++ Vehicle.java
@@ -0,0 +1,18 @@
+class vehicle{
+    private String name;
+    private int engineType;
+
+    vehicle(){
+
+    }
+    public void setName(String name){
+        this.name = name;
+    }public void setEngineType(int engineType){
+        this.engineType = engineType;
+    }public String getName(){
+        return name;
+    }public int getEngineType(){
+        return engineType;
+    }
+
+}
