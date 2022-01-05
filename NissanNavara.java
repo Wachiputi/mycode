d1af510cc6a56a98dd5ec93fe95f412c285501c7
wachiputi <bsc-ele-29-19@unima.ac.mw>
2022-01-05 12:50:19 +0200

    added another class nissanNavara

---
+++ NissanNavara.java
@@ -0,0 +1,14 @@
+class NissanNavara extends vehicle{
+    int model ;
+    int registrationNumber;
+
+    public void setModel(int model){
+        this.model = model;
+    }public void setRegistrationNumber(int registrationNumber){
+        this.registrationNumber = registrationNumber
+    }public int getModel(){
+        return mode;
+    }public int getRegistrationNumber(){
+        return registrationNumber;
+    }
+}
