7e664d300be36bf719594b3d4687e7be84dee86d
Wachiputi <bsc-com-35-19@unima.ac.mw>
2022-01-07 11:59:10 +0200

    we have added some method

--- Overloading.java
+++ Overloading.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class Overloading{
 public static void main(String args[]){
+	Scanner input = new Scanner(System.in);
 	System.out.println("This program show method Overloading");
-		System.out.println("The maximum number between 8 and 5 is "+maxNumber(8,5));
+	System.out.println("please enter 2 numbers");
+	double Number1 = input.nextDouble();
+	double Number2 = input.nextDouble();
+
+		System.out.println("The maximum number between"+Number1+" and "+Number2+" is "+max(Number1,Number2));
 		
 	}
-	public static int maxNumber(int Number1, int Number2){
+	public static int max(int Number1, int Number2){
 		int maxnum;
 		if(Number1>Number2){
 			maxnum = Number1;
@@ -12,5 +18,12 @@ public static void main(String args[]){
 			maxnum = Number2;
 
 			return maxnum;
+	}public static double max(double Number1, double Number2){
+		double maxinum ;
+		if(Number1>Number2){
+			maxinum = Number1;
+		}else
+			maxinum = Number2;
+			return maxinum;
 	}
 }
